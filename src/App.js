817958dc22b2c68700f3885300b0822a68e2541f
KadrioL <adrian.69518@web.de>
2023-04-20 16:52:32 +0200

    added boostrap and styled table and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import Table from './components/Table.js';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './styles.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import CustomTable from './components/Table.js';
 import React, { useState, useEffect } from 'react';
 import { pokemonGen1, pokemonGen2, pokemonGen3, pokemonGen4 } from './components/Table.js';
 
@@ -46,17 +49,43 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <input id="userInput" type="text" autoFocus onKeyUp={() => checkInput(selectedGeneration)} />
-      <select id="generation-select" value={selectedGeneration} onChange={handleGenerationChange}>
-        <option value="1">Generation 1</option>
-        <option value="2">Generation 2</option>
-        <option value="3">Generation 3</option>
-        <option value="4">Generation 4</option>
-      </select>
-      <Table selectedGeneration={selectedGeneration} />
-    </div>
+
+    <Container fluid>
+      <Row>
+        <Col sm={6} className="bg-secondary text-white">
+          <input id="userInput" className="form-control form-control-sm my-2" type="text" autoFocus onKeyUp={() => checkInput(selectedGeneration)} />
+        </Col>
+        <Col sm={6} className="bg-secondary text-white">
+          <select id="generation-select" value={selectedGeneration} onChange={handleGenerationChange}>
+            <option value="1">Generation 1</option>
+            <option value="2">Generation 2</option>
+            <option value="3">Generation 3</option>
+            <option value="4">Generation 4</option>
+          </select>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CustomTable selectedGeneration={selectedGeneration} />
+        </Col>
+      </Row>
+    </Container>
   );
+
+
+  //   <div className="App">
+  //     <div class="form-group mx-sm-3 mb-2 col-sm-6">
+  //       <input id="userInput" className="form-control form-control-sm my-2" type="text" autoFocus onKeyUp={() => checkInput(selectedGeneration)} />
+  //       <select id="generation-select" value={selectedGeneration} onChange={handleGenerationChange}>
+  //         <option value="1">Generation 1</option>
+  //         <option value="2">Generation 2</option>
+  //         <option value="3">Generation 3</option>
+  //         <option value="4">Generation 4</option>
+  //       </select>
+  //   </div>
+  //     <CustomTable selectedGeneration={selectedGeneration}/>
+  //   </div>
+  // );
 }
 
 export default App;
