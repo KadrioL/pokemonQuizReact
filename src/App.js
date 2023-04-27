5cf880db428fbfde33caa0f9c6ec3dbae8989b40
KadrioL <adrian.69518@web.de>
2023-04-27 16:40:41 +0200

    added the .json for every generation, implemented correct checkInput, worked on the second header part to align things right

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import './styles.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import CustomTable from './components/Table.js';
 import React, { useState, useEffect } from 'react';
-import { pokemonGen1, pokemonGen2, pokemonGen3, pokemonGen4 } from './components/Table.js';
 
 function App() {
   const [selectedGeneration, setSelectedGeneration] = useState(1);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const response = await fetch(`pokemonGen${selectedGeneration}.json`);
+      const data = await response.json();
+      setPokemonData(data);
+    };
+    document.getElementById('userInput').value = "";
+    fetchPokemonData();
+  }, [selectedGeneration]);
 
   const handleGenerationChange = (event) => {
     setSelectedGeneration(parseInt(event.target.value));
@@ -19,31 +30,27 @@ function App() {
     for (let i = 0; i < iElements.length; i++) {
       iElements[i].setAttribute('hidden', true);
     }
+    // Counter zurücksetzen
+    setCounter(0);
   }, [selectedGeneration]);
 
-  function checkInput(genValue) {
-    let checkPokemon = [];
-    switch (genValue) {
-      case 1:
-        checkPokemon = pokemonGen1;
-        break;
-      case 2:
-        checkPokemon = pokemonGen2;
-        break;
-      case 3:
-        checkPokemon = pokemonGen3;
-        break;
-      case 4:
-        checkPokemon = pokemonGen4;
-        break;
-      default:
-        checkPokemon = pokemonGen1;
-        break;
-    }
+  function checkInput() {
     const userInput = document.getElementById('userInput').value.toLowerCase();
-    const matchingPokemon = checkPokemon.find(pokemon => pokemon.name.toLowerCase() === userInput);
+    const matchingPokemon = pokemonData.find(pokemon => pokemon.name.toLowerCase() === userInput);
     if (matchingPokemon) {
-      document.getElementById(matchingPokemon.pkdxnr).removeAttribute('hidden');
+      if (matchingPokemon.name.toLowerCase() === 'nidoran') {
+        document.getElementById('29').removeAttribute('hidden');
+        document.getElementById('32').removeAttribute('hidden');
+        setCounter(counter + 2);
+      } else {
+        const matchingElement = document.getElementById(matchingPokemon.pkdxnr);
+        if (matchingElement.getAttribute('hidden') === null) {
+          // Element ist bereits sichtbar, Input nicht löschen
+          return;
+        }
+        matchingElement.removeAttribute('hidden');
+        setCounter(counter + 1);
+      }
       document.getElementById('userInput').value = "";
     }
   }
@@ -52,10 +59,19 @@ function App() {
 
     <Container fluid>
       <Row>
-        <Col sm={6} className="bg-secondary text-white">
-          <input id="userInput" className="form-control form-control-sm my-2" type="text" autoFocus onKeyUp={() => checkInput(selectedGeneration)} />
+        <Col sm={4} className="bg-secondary text-white d-flex justify-content-center align-items-center">
+          <input id="userInput" className="form-control form-control-sm my-2" type="text" autoFocus onKeyUp={() => checkInput()} />
         </Col>
-        <Col sm={6} className="bg-secondary text-white">
+        <Col sm={2} className="bg-secondary text-white d-flex justify-content-center align-items-center">
+          <div>{counter}/{pokemonData.length}</div>
+        </Col>
+        <Col sm={2} className="bg-secondary text-white d-flex justify-content-center align-items-center">
+          <div>Timer</div>
+        </Col>
+        <Col sm={2} className="bg-secondary text-white d-flex justify-content-center align-items-center">
+          <div>SurrenderButton</div>
+        </Col>
+        <Col sm={2} className="bg-secondary text-white d-flex justify-content-center align-items-center">
           <select id="generation-select" value={selectedGeneration} onChange={handleGenerationChange}>
             <option value="1">Generation 1</option>
             <option value="2">Generation 2</option>
@@ -72,20 +88,6 @@ function App() {
     </Container>
   );
 
-
-  //   <div className="App">
-  //     <div class="form-group mx-sm-3 mb-2 col-sm-6">
-  //       <input id="userInput" className="form-control form-control-sm my-2" type="text" autoFocus onKeyUp={() => checkInput(selectedGeneration)} />
-  //       <select id="generation-select" value={selectedGeneration} onChange={handleGenerationChange}>
-  //         <option value="1">Generation 1</option>
-  //         <option value="2">Generation 2</option>
-  //         <option value="3">Generation 3</option>
-  //         <option value="4">Generation 4</option>
-  //       </select>
-  //   </div>
-  //     <CustomTable selectedGeneration={selectedGeneration}/>
-  //   </div>
-  // );
 }
 
 export default App;
